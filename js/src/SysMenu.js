f042b6b75bc3ce1a92c42310fb9fd93f4b707495
Yuvaraj Soundarapandian <yuva0103@gmail.com>
2012-09-05 17:55:54 +0530

    Main menu animation.

--- js/src/SysMenu.js
+++ js/src/SysMenu.js
@@ -1,7 +1,9 @@
 cc.dumpConfig();
  var winSize;
+ var actionScaleTo = cc.ScaleTo.create(1, 2,1.5);
 var SysMenu = cc.Layer.extend({
     _ship:null,
+	
 
     ctor:function () {
         cc.associateWithNative( this, cc.Layer );
@@ -39,23 +41,46 @@ var SysMenu = cc.Layer.extend({
             var creditsNormal = cc.Sprite.create(s_menuButtonCreditsNormal, cc.rect(0, 0, 138, 24));
             var creditsSelected = cc.Sprite.create(s_menuButtonCreditsNormal, cc.rect(0, 0, 138, 24));
             var creditsDisabled = cc.Sprite.create(s_menuButtonCreditsNormal, cc.rect(0, 0 * 2, 138, 24));
-
-            var newGame = cc.MenuItemSprite.create(playNormal, playSelected, playDisabled, this, function () {
+			var actionMoveTo = cc.MoveTo.create(1,cc.p(winSize.width / 2 - 80, winSize.height / 2 - 70));
+			var actionMoveToBack = cc.MoveTo.create(1,cc.p(winSize.width / 2 - 100, winSize.height / 2 - 70));
+			
+            var playGame = cc.MenuItemSprite.create(playNormal, playSelected, playDisabled, this, function () {
                 this.onButtonEffect();
-                this.onNewGame();
+				playGame.runAction(actionScaleTo, null);
+                this.onPlayGame();
                 //flareEffect(this, this, this.onNewGame);
             });
-			var highScore = cc.MenuItemSprite.create(highScoreNormal, highScoreSelected, highScoreDisabled, this, this.onSettings);
-            var options = cc.MenuItemSprite.create(optionsNormal, optionsSelected, optionsDisabled, this, this.onSettings);
-			var instructions = cc.MenuItemSprite.create(instructionsNormal, instructionsSelected, instructionsDisabled, this, this.onSettings);
-            var credits = cc.MenuItemSprite.create(creditsNormal, creditsSelected, creditsDisabled, this, this.onAbout);
-
-            var menu = cc.Menu.create(newGame, highScore, options, instructions, credits);
+			var highScore = cc.MenuItemSprite.create(highScoreNormal, highScoreSelected, highScoreDisabled, this, function () {
+                this.onButtonEffect();
+				highScore.runAction(actionScaleTo, null);
+                this.onHighScore();
+            });
+            var options = cc.MenuItemSprite.create(optionsNormal, optionsSelected, optionsDisabled, this, function () {
+                this.onButtonEffect();
+				options.runAction(actionScaleTo, null);
+                this.onOptions();
+            });
+			var instructions = cc.MenuItemSprite.create(instructionsNormal, instructionsSelected, instructionsDisabled, this, function () {
+                this.onButtonEffect();
+				instructions.runAction(actionScaleTo, null);
+                this.onInstructions();
+            });
+            var credits = cc.MenuItemSprite.create(creditsNormal, creditsSelected, creditsDisabled, this, function () {
+                this.onButtonEffect();
+				credits.runAction(actionScaleTo, null);
+                this.onCredits();
+            });
+				
+			
+			
+		
+            var menu = cc.Menu.create(playGame, highScore, options, instructions, credits);
             menu.alignItemsVerticallyWithPadding(10);
             this.addChild(menu, 1, 2);
-			menu.setPosition(cc.p(winSize.width / 2 - 100, winSize.height / 2 - 70));
-            //menu.setPosition(cc.p(winSize.width / 2, winSize.height / 2 - 80));
+			menu.setPosition(cc.p(winSize.width / 2 - 200, winSize.height / 2 - 70));
+			menu.runAction(cc.Sequence.create(actionMoveTo,actionMoveToBack,null));
             this.schedule(this.update, 0.1);
+			
 
             if (MW.SOUND) {
                 cc.AudioEngine.getInstance().setBackgroundMusicVolume(0.7);
@@ -67,19 +92,27 @@ var SysMenu = cc.Layer.extend({
 
         return bRet;
     },
-    onNewGame:function (pSender) {
-        var scene = cc.Scene.create();
+    onPlayGame:function (pSender) {
+	    var scene = cc.Scene.create();
         scene.addChild(GameEngine.create());
-        cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1.2, scene));
+		cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1.2, scene));
     },
-    onSettings:function (pSender) {
-        this.onButtonEffect();
+	onHighScore:function (pSender) {
         var scene = cc.Scene.create();
         scene.addChild(SettingsLayer.create());
         cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1.2, scene));
     },
-    onAbout:function (pSender) {
-        this.onButtonEffect();
+    onOptions:function (pSender) {
+        var scene = cc.Scene.create();
+        scene.addChild(SettingsLayer.create());
+        cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1.2, scene));
+    },
+	onInstructions:function (pSender) {
+        var scene = cc.Scene.create();
+        scene.addChild(SettingsLayer.create());
+        cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1.2, scene));
+    },
+    onCredits:function (pSender) {
         var scene = cc.Scene.create();
         scene.addChild(AboutLayer.create());
         cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1.2, scene));
